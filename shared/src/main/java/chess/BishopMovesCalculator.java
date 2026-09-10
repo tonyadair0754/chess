5f67cc46553bc8f83b9841937621b62d99d12daf
Tony Adair <tonyadair0754@gmail.com>
2026-09-09 20:03:35 -0600

    Add BishopMovesCalculator and QueenMovesCalculator

---
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -0,0 +1,61 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class BishopMovesCalculator implements PieceMovesCalculator {
+
+    /**
+     * Defines where a bishop can move
+     *
+     * @param board
+     * @param position
+     * @return A list of ChessMove objects
+     */
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
+        List<ChessMove> moves = new ArrayList<>();
+
+        checkDirection(1, 1, moves, board, position);
+        checkDirection(1, -1, moves, board, position);
+        checkDirection(-1, 1, moves, board, position);
+        checkDirection(-1, -1, moves, board, position);
+
+        return moves;
+    }
+
+    /**
+     * Helper method that checks where the bishop can move in one direction
+     *
+     * @param rowChange
+     * @param colChange
+     * @param moves
+     * @param board
+     * @param position
+     */
+    private void checkDirection(int rowChange, int colChange, List<ChessMove> moves, ChessBoard board, ChessPosition position) {
+        ChessPiece bishop = board.getPiece(position);
+        int row = position.getRow();
+        int col = position.getColumn();
+
+        while ((row + rowChange < 9 && row + rowChange > 0) && (col + colChange < 9 && col + colChange > 0)) {
+            row += rowChange;
+            col += colChange;
+            ChessPosition newPos = new ChessPosition(row, col);
+            ChessPiece piece = board.getPiece(newPos);
+
+            if (piece == null) {
+                moves.add(new ChessMove(position, newPos, null));
+                continue;
+            }
+
+            if (piece.getTeamColor() != bishop.getTeamColor()) {
+                moves.add(new ChessMove(position, newPos, null));
+                break;
+            } else {
+                break;
+            }
+        }
+    }
+}
